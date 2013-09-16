c0fa488e0d9ceb802e72eb8c6ef98573abafa30e
cmarks-msw <cmarks@magpiesw.com>
2013-09-15 22:49:56 -0600

    Add initial files to the project.

---
+++ src/main/java/com/example/MyObject.java
@@ -0,0 +1,12 @@
+package com.example;
+
+/**
+ * Sample class file for the Gradle examples.
+ */
+public class MyObject
+{
+    public void doSomething(){
+        System.out.println("Hello, world");
+    }
+
+}
